d66856f4233f02cbc40b61b85741e06f7460fd99
jdanielhoyos <58446071+jdanielhoyos@users.noreply.github.com>
2022-10-23 02:10:48 -0400

    Functions

---
+++ yolov4ObjectDetectionExample.m
@@ -0,0 +1,63 @@
+clear all
+close all
+clc
+mycam = webcam(1);
+
+%% Object Detection Using Pretrained YOLO v4 Network
+% The following code demonstrates running object detection using a pretrained 
+% YOLO v4 network, trained on COCO dataset.
+
+%% Prerequisites
+% To run this example you need the following prerequisites - 
+%
+% # MATLAB (R2021a or later) with Computer Vision and Deep Learning Toolbox.
+% # Pretrained YOLOv4 network (download instructions below).
+
+%% Setup
+% Add path to the source directory.
+addpath('src');
+
+%% Download the pre-trained network
+% This repository uses two variants of YOLO v4 models.
+% *YOLOv4-coco*
+% *YOLOv4-tiny-coco*
+% Set the modelName from the above ones to download that pretrained model.
+modelName = 'YOLOv4-coco';
+model = helper.downloadPretrainedYOLOv4(modelName);
+net = model.net;
+
+%% Perform Object Detection Using YOLOv4 Network
+% Inference is performed on the pretrained YOLOv4 network. 
+% Read test image.
+
+figure
+while 1    
+    image = snapshot(mycam);
+
+
+    %image = imread('visionteam.jpg');
+    
+    % Get classnames of COCO dataset.
+    classNames = helper.getCOCOClassNames;
+    
+    % Get anchors used in training of the pretrained model.
+    anchors = helper.getAnchors(modelName);
+    
+    % Detect objects in test image.
+    executionEnvironment = 'auto';
+    [bboxes, scores, labels] = detectYOLOv4(net, image, anchors, classNames, executionEnvironment);
+    
+    % Visualize detection results.
+    annotations = string(labels) + ": " + string(scores);
+    if  ~isempty(bboxes)
+        image = insertObjectAnnotation(image, 'rectangle', bboxes, annotations, 'FontSize', 30);
+    end
+    imshow(image)
+    pause(0.001);
+
+    
+end
+
+
+
+
